20d9a2b9496383de45e61736f0d98e96bcd33222
Carlos Espinoza <ce@mediaimpact.pe>
2017-02-06 16:09:12 -0500

    agragando funcionalidades

---
+++ controller/usuario/salir.php
@@ -0,0 +1,16 @@
+<?php
+header('Content-Type: application/json; charset=UTF-8');
+date_default_timezone_set('America/Lima');//-->definimos la zona horaria
+include('../../config/conexion.php');
+include('../../modelo/funciones.php');
+include('../../modelo/usuario.php');
+
+$arrjson['id']    = $_SESSION['COD_USER'];
+$objuser = new Usuario();
+$objuser->CerrarSession($arrjson['id']);
+
+$objfuncion = new misFunciones();
+$objfuncion->elminarSession();
+
+echo json_encode($arrjson);
+?>
